8d8d464ec8b99a6802f3a296137934b7dc1cba15
Amy Y. Olsen <greenamy808@gmail.com>
2021-04-16 10:14:00 -0700

    Added spooky app with widgets and outputs

--- app.R
+++ app.R
@@ -1,1 +1,58 @@
 #R Shiny tutorial
+
+# Attach necessary packages
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+
+spooky <- read_csv("spooky_data.csv")
+
+# Create the user interface:
+ui <- fluidPage(
+  theme = shinytheme("slate"),
+  titlePanel("Spooky Shiny App!"),
+  sidebarLayout(
+    sidebarPanel("Widgets!",
+                 selectInput(inputId = "state_select",
+                             label = "Choose a state:",
+                             choices = unique(spooky$state)),
+                 radioButtons(inputId = "region_select",
+                              label = "Choose region:",
+                              choices = unique(spooky$region_us_census))),
+    mainPanel("Outputs!",
+              p("State's top candies:"),
+              tableOutput(outputId = "candy_table"),
+              p("Region's top costumes:",
+                plotOutput(outputId = "costume_graph")))
+  )
+)
+
+# Create the server function:
+server <- function(input, output) {
+  
+  state_candy <- reactive({
+    spooky %>%
+      filter(state == input$state_select) %>%
+      select(candy, pounds_candy_sold)
+  })
+  
+  output$candy_table <- renderTable({
+    state_candy()
+  })
+  region_costume <- reactive({
+    spooky %>%
+      filter(region_us_census == input$region_select) %>%
+      count(costume, rank)
+  })
+  output$costume_graph <- renderPlot({
+    ggplot(region_costume(), aes(x = costume, y = n)) +
+      geom_col(aes(fill = rank)) +
+      coord_flip() +
+      scale_fill_manual(values = c("black", "purple", "orange")) +
+      theme_minimal()
+  })
+}
+
+
+# Combine them into an app:
+shinyApp(ui = ui, server = server)
